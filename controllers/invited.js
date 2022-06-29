5188861781955d5cdd403f41f9d837ba499e9b6e
Carlos <carlos@Mac-mini-de-Carlos.local>
2022-06-29 00:36:25 -0500

    Internal validator for pases confirmados done

--- controllers/invited.js
+++ controllers/invited.js
@@ -18,15 +18,19 @@ const invitedPut = async (req, res = response) => {
     const { id } = req.params;
     const { body } = req
     try {
-    const usuario = await Usuario.findByIdAndUpdate(id, body);
-    if(usuario){
-        res.json({usuario})
+    const validName = await Usuario.findById(id)
+    if(!validName.confirmados){
+        const usuario = await Usuario.findByIdAndUpdate(id, body); 
+        res.json({usuario}) 
+    } else{
+        res.status(401).json({message: 'Estos pases ya fueron confirmados'})
     }
     } catch (error) {
         res.status(400).json(error)
     }
     
 }
+
 
 const invitedPost = async (req, res = response) => {
     const body = req.body
